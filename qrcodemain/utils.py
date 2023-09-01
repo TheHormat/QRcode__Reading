6477925be59f8304e08a57a8d74e97d4a2e3a193
Hormat Hamidov <88937250+HormatHamidov@users.noreply.github.com>
2023-09-01 20:08:09 +0400

    Update utils.py

--- qrcodemain/utils.py
+++ qrcodemain/utils.py
@@ -5,5 +5,5 @@ def create_qr_code_image(url, qr_id):
     qr_image = qrcode.make(url)
     qr_image_path = f"static/qr_codes/{qr_id}.png"
     qr_image.save(qr_image_path)
-    return qr_image_path  # QR kodunun yolu döndürülüyor.
+    return qr_image_path  
 
